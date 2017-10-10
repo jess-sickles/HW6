37c2dc703af5abf1ed4a20bb62befdedd1003a22
Jess Sickles <jasickle@umich.edu>
2017-10-09 23:42:28 -0400

    Working code

--- partA.py
+++ partA.py
@@ -1,6 +1,7 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import ssl 
+import re
 
 ctx = ssl.create_default_context()
 ctx.check_hostname = False
@@ -8,6 +9,10 @@ ctx.verify_mode = ssl.CERT_NONE
 
 url = input('Enter url: ')
 html = urlopen(url, context = ctx).read()
-tags = soup("span")
+soup = BeautifulSoup(html, "html.parser")
+tags = soup('span')
+nums = []
 for tag in tags:
-	print('Number:,'tag.get('span'),None)
+	nums.append(int((tag.contents[0])))
+print(sum(nums))
+	
